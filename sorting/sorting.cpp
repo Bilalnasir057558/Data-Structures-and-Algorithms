41d640e25d80f6693a33448898c3fb79a361aa6d
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-08-25 12:25:48 +0500

    add insertion sort implementation

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -63,11 +63,30 @@ void bubbleSort(vector<int>& nums) {
     };
 }
 
+void insertionSort(vector<int>& nums) {
+
+    int n = nums.size();
+    for(int i = 1; i < n; i++) {
+        int key = nums[i];
+        int j = i - 1;
+
+        while(j >= 0 && nums[j] > key) {
+
+            // shift larger element to the right to free up the space
+            nums[j + 1] = nums[j];
+            j--;
+        }
+
+        nums[j + 1] = key;
+    }
+}
+
 int main() {
 
     vector<int> v = {5, 4, 3, 2, 1};
     // selectionSort(v);
-    bubbleSort(v);
+    // bubbleSort(v);
+    insertionSort(v);
 
     for(auto el : v) {
         cout<< el << " ";
